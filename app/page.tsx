0d15c6feba7fd73d87c45f6a619328df13d9dd74
Matías Barrios <matias.barriosn@gmail.com>
2025-06-30 01:01:44 -0400

    comunidades

--- app/page.tsx
+++ app/page.tsx
@@ -32,7 +32,7 @@ export default function Home() {
           href="#"
           className="flex h-12 w-full items-center justify-center rounded-md border border-solid border-black/[.08] bg-white text-center text-base font-medium text-black transition-colors hover:bg-gray-100 dark:border-white/[.145] dark:bg-black dark:text-white dark:hover:bg-gray-900"
         >
-          Organizaciones
+          Comunidades
         </a>
       </div>
     </main>
